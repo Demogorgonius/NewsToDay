7d7604f64f49008bf98aedb18d9a36a52fe7cfa1
«Daniil <danilkulikovskiy@gmail.com>
2023-05-10 19:42:55 +0300

    Add app icon, finish NewsViewController layout

--- NewsToDay/Controller/NewsViewConroller.swift
+++ NewsToDay/Controller/NewsViewConroller.swift
@@ -20,7 +20,9 @@ class NewsViewConroller: UIViewController {
     private let pictureNews: UIImageView = {
        let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
-        image.backgroundColor = .red
+        image.backgroundColor = .blue
+        image.image = UIImage(named: "default")
+        image.contentMode = .scaleAspectFill
         return image
     }()
     
@@ -38,12 +40,63 @@ class NewsViewConroller: UIViewController {
         return contentView
     }()
     
-    private let textDiscription: UILabel = {
+    private let titleLabel: UILabel = {
+        let text = UILabel()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.numberOfLines = 0
+        text.font = UIFont(name: "Inter-Bold", size: 24)
+        text.textColor = UIColor(named: Resources.Colors.white)
+//        text.setLineHeight(30)
+        text.text = "The latest situation in the presidential election"
+        text.textAlignment = .left
+        return text
+    }()
+    
+    private let autorName: UILabel = {
+        let text = UILabel()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.numberOfLines = 0
+        text.font = UIFont(name: "Inter-Bold", size: 24)
+        text.textColor = UIColor(named: Resources.Colors.white)
+        text.setLineHeight(24)
+        text.text = "John Doe"
+        return text
+    }()
+    
+    private let autor: UILabel = {
         let text = UILabel()
         text.translatesAutoresizingMaskIntoConstraints = false
         text.numberOfLines = 0
         text.font = UIFont(name: "Inter", size: 18)
-//        text.font = .systemFont(ofSize: 22, weight: .regular)
+        text.textColor = UIColor(named: Resources.Colors.greyLight)
+//        text.setLineHeight(20)
+        text.text = "Author"
+        return text
+    }()
+    
+    private let category: UILabel = {
+        let text = UILabel()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.numberOfLines = 0
+        text.font = UIFont(name: "Inter", size: 14)
+        text.textColor = UIColor(named: Resources.Colors.greyLight)
+//        text.setLineHeight(20)
+        text.text = "Politics"
+        text.textColor = .white
+        text.textAlignment = .center
+        text.backgroundColor = UIColor(named: Resources.Colors.purplePrimary)
+        text.layer.cornerRadius = 16
+        text.layer.masksToBounds = true
+        return text
+    }()
+    
+    private let textDiscription: UILabel = {
+        let text = UILabel()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.numberOfLines = 0
+        text.font = UIFont(name: "Inter", size: 20)
+        text.textColor = UIColor(named: Resources.Colors.greyDarker)
+        text.setLineHeight(24)
         text.text = "Leads in individual states may change from one party to another as all the votes are counted. Select a state for detailed results, and select the Senate, House or Governor tabs to view those races. For more detailed state results click on the States A-Z links at the bottom of this page. Results source: NEP/Edison via Reuters. Leads in individual states may change from one party to another as all the votes are counted. Select a state for detailed results, and select the Senate, House or Governor tabs to view those races. For more detailed state results click on the States A-Z links at the bottom of this page. Results source: NEP/Edison via Reuters."
         return text
     }()
@@ -54,6 +107,10 @@ extension NewsViewConroller {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         contentView.addSubview(pictureNews)
+        contentView.addSubview(category)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(autorName)
+        contentView.addSubview(autor)
         contentView.addSubview(textDiscription)
         
         
@@ -70,14 +127,32 @@ extension NewsViewConroller {
             contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
             
-            pictureNews.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            pictureNews.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            pictureNews.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            pictureNews.heightAnchor.constraint(equalToConstant: 350),
+            pictureNews.topAnchor.constraint(equalTo: contentView.topAnchor),
+            pictureNews.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            pictureNews.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            pictureNews.heightAnchor.constraint(equalToConstant: 360),
             
-            textDiscription.topAnchor.constraint(equalTo: pictureNews.bottomAnchor, constant: 24),
-            textDiscription.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
-            textDiscription.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+            category.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -16),
+            category.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            category.widthAnchor.constraint(equalToConstant: 75),
+            category.heightAnchor.constraint(equalToConstant: 35),
+            
+            titleLabel.bottomAnchor.constraint(equalTo: autorName.topAnchor, constant: -24),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 60),
+            
+            autorName.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+            autorName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 26),
+//            autorName.bottomAnchor.constraint(equalTo: pictureNews.bottomAnchor, constant: -48),
+            
+            autor.topAnchor.constraint(equalTo: autorName.bottomAnchor),
+            autor.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 26),
+            autor.bottomAnchor.constraint(equalTo: pictureNews.bottomAnchor, constant: -24),
+            
+            textDiscription.topAnchor.constraint(equalTo: pictureNews.bottomAnchor, constant: 44),
+            textDiscription.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            textDiscription.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             textDiscription.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -15)
         ])
 
