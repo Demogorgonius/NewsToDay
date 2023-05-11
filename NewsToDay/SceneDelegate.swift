8fb85ea55dd82fb94daa6daf194d89684a2ead8c
Mark Raitman <markraitman@mail.ru>
2023-05-11 23:09:12 +0300

    OnboardingViewController
    Add OnboardingViewController wtih UI, constraints and logic

--- NewsToDay/SceneDelegate.swift
+++ NewsToDay/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
                window = UIWindow(windowScene: scene)
                //let vc = ViewController()
-               let vcNews = NewsViewConroller()
+               let vcNews = OnboardingViewController()
             let vc = CustomTabBarController()
             window!.rootViewController = vcNews
             window!.makeKeyAndVisible()
