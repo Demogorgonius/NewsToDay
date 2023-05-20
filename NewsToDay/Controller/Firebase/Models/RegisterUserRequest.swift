b76f3dae6d636002305c72f5c1651099d4b48e0f
Mark Raitman <markraitman@mail.ru>
2023-05-20 16:54:28 +0300

    RegisterUserRequest
    Add RegisterUserRequest model for the RegistrationController

---
+++ NewsToDay/Controller/Firebase/Models/RegisterUserRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterUserRequest.swift
+//  NewsToDay
+//
+//  Created by Марк Райтман on 20.05.2023.
+//
+
+import Foundation
+
+struct RegisterUserRequest {
+    let userName: String
+    let email: String
+    let password: String
+}
