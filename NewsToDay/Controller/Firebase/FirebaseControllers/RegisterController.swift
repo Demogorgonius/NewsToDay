a0f9dea0b62ebf0e61249089aa82cc805cdac538
Mark Raitman <markraitman@mail.ru>
2023-05-20 16:46:24 +0300

    RegisterController
    Add RegisterController with UI, methods and Firebase service

---
+++ NewsToDay/Controller/Firebase/FirebaseControllers/RegisterController.swift
@@ -0,0 +1,190 @@
+//
+//  RegisterController.swift
+//  NewsToDay
+//
+//  Created by Марк Райтман on 20.05.2023.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+    
+    //MARK: - UI Components
+    private let headerView = AuthHeaderView(title: "Sign Up", subtitle: "Create your account")
+    
+    private let usernameField = CustomTextField(fieldType: .username)
+    private let emailField = CustomTextField(fieldType: .email)
+    private let passwordField = CustomTextField(fieldType: .password)
+    
+    private let signUpButton = CustomButton(title: "Sign Up", hasBackground: true, fontSize: .big)
+    private let signInButton = CustomButton(title: "Already have an account? Sign in", hasBackground: false, fontSize: .med)
+    
+    /// textView
+    private let termsTextView: UITextView = {
+        let attributedText = NSMutableAttributedString(string: "By creating account, you agree to our Terms & Conditions and you acknowledge that have read our Privacy Policy")
+        attributedText.addAttribute(.link, value: "terms://termsAndConditions", range: (attributedText.string as NSString).range(of: "Terms & Conditions"))
+        attributedText.addAttribute(.link, value: "privacy://privacyPolicy", range: (attributedText.string as NSString).range(of: "Privacy Policy"))
+        let textView = UITextView()
+        textView.linkTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemBlue]
+        textView.attributedText = attributedText
+        textView.backgroundColor = .clear
+        textView.textColor = .label
+        textView.delaysContentTouches = false
+        textView.isSelectable = true
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        buttonsTapped()
+        
+        termsTextView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    //MARK: - Methods
+    private func buttonsTapped() {
+        signUpButton.addTarget(self, action: #selector(didTapSignUp), for: .touchUpInside)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+    }
+    
+    @objc private func didTapSignUp() {
+        let registerUserRequest = RegisterUserRequest(userName: usernameField.text ?? "",
+                                                      email: emailField.text ?? "",
+                                                      password: passwordField.text ?? ""
+        )
+        
+        // Username check
+        if !Validator.isValidUsername(for: registerUserRequest.userName) {
+            AlertManager.showInvalidUsernameAlert(on: self)
+            return
+        }
+        
+        // Email check
+        if !Validator.isValidEmail(for: registerUserRequest.email) {
+            AlertManager.showInvalidEmailAlert(on: self)
+            return
+        }
+        
+        // Password check
+        if !Validator.isPasswordValid(for: registerUserRequest.password) {
+            AlertManager.showInvalidPasswordAlert(on: self)
+            return
+        }
+        
+        AuthService.shared.registerUser(with: registerUserRequest) { [weak self] wasRegistered, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                AlertManager.showRegistrationErrorAlert(on: self, with: error)
+                return
+            }
+            
+            if wasRegistered {
+                let vc = OnboardingViewController()
+                self.navigationController?.pushViewController(vc, animated: true)
+            } else {
+                AlertManager.showRegistrationErrorAlert(on: self)
+            }
+        }
+    }
+    @objc private func didTapSignIn() {
+        navigationController?.popToRootViewController(animated: true)
+        print(#function)
+    }
+    
+    //MARK: - UI Setup
+    func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        /// ADDING SUBVIEWS
+        view?.addSubview(headerView)
+        view?.addSubview(usernameField)
+        view?.addSubview(emailField)
+        view?.addSubview(passwordField)
+        view?.addSubview(signUpButton)
+        view?.addSubview(termsTextView)
+        view?.addSubview(signInButton)
+        
+        /// TAMIC
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        usernameField.translatesAutoresizingMaskIntoConstraints = false
+        emailField.translatesAutoresizingMaskIntoConstraints = false
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        termsTextView.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        /// SETUP CONSTRAINTS
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 220),
+            
+            usernameField.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 15),
+            usernameField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            usernameField.heightAnchor.constraint(equalToConstant: 55),
+            usernameField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            emailField.topAnchor.constraint(equalTo: usernameField.bottomAnchor, constant: 15),
+            emailField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            emailField.heightAnchor.constraint(equalToConstant: 55),
+            emailField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 15),
+            passwordField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            passwordField.heightAnchor.constraint(equalToConstant: 55),
+            passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            signUpButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 15),
+            signUpButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            signUpButton.heightAnchor.constraint(equalToConstant: 44),
+            signUpButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            termsTextView.topAnchor.constraint(equalTo: signUpButton.bottomAnchor, constant: 15),
+            termsTextView.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            termsTextView.heightAnchor.constraint(equalToConstant: 55),
+            termsTextView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            signInButton.topAnchor.constraint(equalTo: termsTextView.bottomAnchor, constant: 15),
+            signInButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            signInButton.heightAnchor.constraint(equalToConstant: 55),
+            signInButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            
+        ])
+        
+    }
+}
+
+extension RegisterController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        if URL.scheme == "terms" {
+            showWebViewController(with: "https://policies.google.com/terms?hl=en")
+        } else if URL.scheme == "privacy" {
+            showWebViewController(with: "https://policies.google.com/privacy?hl=en")
+        }
+        return true
+    }
+    
+    private func showWebViewController(with urlString: String) {
+        let vc = WebViewController(with: urlString)
+        let nav = UINavigationController(rootViewController: vc)
+        present(nav, animated: true, completion: nil)
+    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        textView.delegate = nil
+        textView.selectedTextRange = nil
+        textView.delegate = self
+    }
+}
