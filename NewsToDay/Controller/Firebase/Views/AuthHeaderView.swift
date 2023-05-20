f4cdd61933b3759a3b54af8398fdfd34f73dbed3
Mark Raitman <markraitman@mail.ru>
2023-05-20 16:16:57 +0300

    AuthHeaderView
    Add custom AuthHeaderView

---
+++ NewsToDay/Controller/Firebase/Views/AuthHeaderView.swift
@@ -0,0 +1,86 @@
+//
+//  AuthHeaderView.swift
+//  NewsToDay
+//
+//  Created by Марк Райтман on 20.05.2023.
+//
+
+import UIKit
+
+class AuthHeaderView: UIView {
+    
+    //MARK: - UI Components
+    /// image
+    private let logoImageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.image = UIImage(named: "city_1")
+        image.layer.cornerRadius = 25
+        image.layer.masksToBounds = true
+        return image
+    }()
+    
+    /// label
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Error"
+        label.textColor = .label
+        label.font = UIFont.boldSystemFont(ofSize: 26)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    /// label
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Error"
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+    init(title: String, subtitle: String) {
+        super.init(frame: .zero)
+        self.titleLabel.text = title
+        self.subTitleLabel.text = subtitle
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - UI Setup
+    func setupUI() {
+        
+        /// ADDING SUBVIEWS
+        addSubview(logoImageView)
+        addSubview(titleLabel)
+        addSubview(subTitleLabel)
+        
+        /// TAMIC
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        /// SETUP CONSTRAINTS
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: topAnchor, constant: 30),
+            logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 90),
+            logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            subTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            subTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        
+    }
+}
